75ba49ba9d25e6ae29cacd5687cdacfb4d4a6a44
joe miller <joeym@joeym.net>
2016-02-18 16:35:55 -0700

    first commit of a working formula

--- Formula/creds.rb
+++ Formula/creds.rb
@@ -1,15 +1,17 @@
 class Creds < Formula
   desc "Simple encrypted credential file management with GPG"
   homepage "https://github.com/joemiller/creds"
-  url "//github.com/joemiller/creds/archive/master.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/joemiller/creds/archive/v0.0.3.tar.gz"
+  sha256 "f7cf9b3c9bfad516fc01004e6791a20f899cf58502623fc0eb353b6433fd07f5"
+
+	head "https://github.com/joemiller/creds.git"
 
   bottle :unneeded
 
   def install
     bin.install "creds"
-		zsh_completion "completions/zsh/_creds.sh"
-		bash_completion "completions/bash/_creds.sh"
+		zsh_completion.install "completions/zsh/_creds.sh"
+		bash_completion.install "completions/bash/_creds.sh"
   end
 
   test do
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-TODO
+joemiller/homebrew-taps
+=======================
+
+[Homebrew](http://brew.sh) (OSX) Taps for installing various packages.
+
+
+Available packages ("Formula")
+--------------------
+
+- [creds](https://github.com/joemiller/creds): Simple encrypted credential file management with GPG.
+
+Usage
+-----
+
+All taps are in the `Formula` subdirectory and can be installed via:
+
+```
+$ brew install joemiller/taps/<formula-name>
+```
+
+Or:
+
+```
+$ brew tap joemiller/taps
+$ brew install <formula-name>
+```
