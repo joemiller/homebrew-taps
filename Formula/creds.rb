5ef19ec86d0041817d98ff795fd3fdc0628c4cdb
joe miller <joeym@joeym.net>
2017-04-02 07:35:16 -0700

    bump creds to 0.1.0

--- Formula/creds.rb
+++ Formula/creds.rb
@@ -1,20 +1,20 @@
 class Creds < Formula
-  desc "Simple encrypted credential management with GPG"
-  homepage "https://github.com/joemiller/creds"
-  url "https://github.com/joemiller/creds/archive/v0.0.4.tar.gz"
-  sha256 "7ce70ac27916dc9ae541ca81ea574a2064eb883adf74ff06b8dc155cda411c80"
+  desc 'Simple encrypted credential management with GPG'
+  homepage 'https://github.com/joemiller/creds'
+  url 'https://github.com/joemiller/creds/archive/v0.1.0.tar.gz'
+  sha256 'ce322fd63296b4ef045b777d1b42ce25290ced053741aadaf8bb6979685c451a'
 
-	head "https://github.com/joemiller/creds.git"
+  head 'https://github.com/joemiller/creds.git'
 
   bottle :unneeded
 
   def install
-    bin.install "creds"
-		zsh_completion.install "completions/zsh/_creds.sh"
-		bash_completion.install "completions/bash/_creds.sh"
+    bin.install 'creds'
+    zsh_completion.install 'completions/zsh/_creds.sh'
+    bash_completion.install 'completions/bash/_creds.sh'
   end
 
   test do
-    system "#{bin}/creds", "--version"
+    system "#{bin}/creds", '--version'
   end
 end
