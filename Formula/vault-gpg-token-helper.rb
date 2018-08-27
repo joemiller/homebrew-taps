1a0ec7af7c6007518203900c321a0bf4b91616f2
goreleaser <goreleaser@joemiller.me>
2018-08-26 17:25:19 -0700

    Brew formula update for vault-gpg-token-helper version v0.1.0

--- Formula/vault-gpg-token-helper.rb
+++ Formula/vault-gpg-token-helper.rb
@@ -1,11 +1,19 @@
 class VaultGpgTokenHelper < Formula
   desc "A @hashicorp Vault token helper for GPG (+ yubikey)"
   homepage "https://github.com/joemiller/vault-gpg-token-helper"
-  url "https://github.com/joemiller/vault-gpg-token-helper/releases/download/v0.0.2/vault-gpg-token-helper_0.0.2_darwin_amd64.tar.gz"
-  version "0.0.2"
-  sha256 "8e4ad67b4fb8ddcd0e00958d51711fe9b43ba3b2c80d543bb65447cfd1b5568a"
+  url "https://github.com/joemiller/vault-gpg-token-helper/releases/download/v0.1.0/vault-gpg-token-helper_0.1.0_darwin_amd64.tar.gz"
+  version "0.1.0"
+  sha256 "4dc486ed19fe06161d4e949d1632e7741ca37bc15bb7f727e8f8fd1f3bb2b69f"
 
   def install
     bin.install "vault-gpg-token-helper"
+  end
+
+  def caveats; <<~EOS
+    You need to set a path to the token helper in your ~/.vault file.
+    Run the following to create the ~/.vault file:
+    
+    echo "token_helper = \"$(brew --prefix joemiller/taps/vault-gpg-token-helper)/bin/vault-gpg-token-helper\"" > ~/.vault
+  EOS
   end
 end
