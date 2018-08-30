cd019a019634c6b175081f8b6cdfc56acc4c205e
goreleaser <goreleaser@joemiller.me>
2018-08-30 14:07:23 -0700

    Brew formula update for vault-gpg-token-helper version v0.1.3

--- Formula/vault-gpg-token-helper.rb
+++ Formula/vault-gpg-token-helper.rb
@@ -1,9 +1,9 @@
 class VaultGpgTokenHelper < Formula
   desc "A @hashicorp Vault token helper for GPG (+ yubikey)"
   homepage "https://github.com/joemiller/vault-gpg-token-helper"
-  url "https://github.com/joemiller/vault-gpg-token-helper/releases/download/v0.1.2/vault-gpg-token-helper_0.1.2_darwin_amd64.tar.gz"
-  version "0.1.2"
-  sha256 "20ce7cb3dfcaac68765f88ee7b27847e3bc4ae0b6500c42d0ab7041e3cb2e27b"
+  url "https://github.com/joemiller/vault-gpg-token-helper/releases/download/v0.1.3/vault-gpg-token-helper_0.1.3_darwin_amd64.tar.gz"
+  version "0.1.3"
+  sha256 "5e5f068834c3633e6e16f982efb79658bf734610ab5b5cbf0f59c599045f13f8"
 
   def install
     bin.install "vault-gpg-token-helper"
