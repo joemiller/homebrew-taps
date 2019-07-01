287aa27a9c9ed4c4d03f9fb000d08183ea8d37a8
joe miller <goreleaser@joemiller.me>
2019-07-01 09:00:24 -0700

    Brew formula update for vault-token-helper version v0.0.5

--- Formula/vault-token-helper.rb
+++ Formula/vault-token-helper.rb
@@ -2,14 +2,14 @@
 class VaultTokenHelper < Formula
   desc "Cross-platform @hashicorp Vault token helper"
   homepage "https://github.com/joemiller/vault-token-helper"
-  version "0.0.3"
+  version "0.0.5"
 
   if OS.mac?
-    url "https://github.com/joemiller/vault-token-helper/releases/download/v0.0.3/vault-token-helper_0.0.3_darwin_amd64.tar.gz"
-    sha256 "e031708cf6761c1d5786e016c8ae552973a2052df675d1af1b313f8fdfb2b0c6"
+    url "https://github.com/joemiller/vault-token-helper/releases/download/v0.0.5/vault-token-helper_0.0.5_darwin_amd64.tar.gz"
+    sha256 "6f7cd780cb9fe9553fd7e7bc18353f69919484c5b86f05e9b7f9ecd24bf3023c"
   elsif OS.linux?
-    url "https://github.com/joemiller/vault-token-helper/releases/download/v0.0.3/vault-token-helper_0.0.3_linux_amd64.tar.gz"
-    sha256 "6f1655ec70ba9102ee8398f70d7d63e421e7cb0f839230fb0d8819476d1757b6"
+    url "https://github.com/joemiller/vault-token-helper/releases/download/v0.0.5/vault-token-helper_0.0.5_linux_amd64.tar.gz"
+    sha256 "c8eb57aae6ff55f1e169a35341370c23c329a54c4ae6ac0c22e59094673d633e"
   end
   
   depends_on "vault"
