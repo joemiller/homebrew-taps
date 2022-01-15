537762576beb3db4019d75ef2db82433181cd9cb
joe miller <joeym@joeym.net>
2022-01-15 08:36:16 -0800

    remove deprecated option

--- Formula/docker-tail.rb
+++ Formula/docker-tail.rb
@@ -4,9 +4,7 @@ class DockerTail < Formula
   url 'https://github.com/joemiller/docker-tail/archive/v0.1.0.tar.gz'
   sha256 '7171e2ef7f24d4bbc722cec781ffe62b8725106b259fcc6472c2ff07af817b7b'
 
-	head 'https://github.com/joemiller/docker-tail.git'
-
-  bottle :unneeded
+  head 'https://github.com/joemiller/docker-tail.git'
 
   def install
     bin.install 'docker-tail'
